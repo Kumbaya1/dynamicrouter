a4d104996c9b14a5a0bf38c9030d1d81aa8c77ec
Kumbaya1 <790023580@qq.com>
2019-06-06 19:36:42 +0800

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-button type="primary" @click="login">登录</el-button>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  methods: {
+    login() {
+      Axios.get("https://www.easy-mock.com/mock/5cf8df38318e3421d8bb13a8/example/getbaserouters").then(res=>{
+        const routers = res.data.data.router;
+        console.log(routers)
+      })
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
